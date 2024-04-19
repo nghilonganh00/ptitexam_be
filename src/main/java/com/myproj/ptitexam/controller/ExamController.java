4119644ccef0db78e42b56c5e5f0a42c734b5145
anhtu010203 <TuNA.B21DCCN747@stu.ptit.edu.vn>
2024-04-19 18:25:24 +0700

    Thêm model Question

--- src/main/java/com/myproj/ptitexam/controller/ExamController.java
+++ src/main/java/com/myproj/ptitexam/controller/ExamController.java
@@ -3,11 +3,11 @@ package com.myproj.ptitexam.controller;
 
 import com.myproj.ptitexam.model.Exam;
 import com.myproj.ptitexam.model.Question;
+import com.myproj.ptitexam.model.User;
 import com.myproj.ptitexam.service.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +22,9 @@ public class ExamController {
         return examService.getAllExams();
     }
 
+    @PostMapping("createExam")
+    public ResponseEntity<String> createExam(@RequestBody Exam exam){
+        return examService.createExam(exam);
+    }
 
 }
